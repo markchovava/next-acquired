c5ebf57d645bf2ae239a70799a62ed8385f71146
Mark Chovava <markchovava@gmail.com>
2025-03-19 19:30:43 +0200

    Tets'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import Home from './_components/Home';
 import { businessListAction } from '@/actions/BusinessActions';
 import { cityListAllAction } from '@/actions/CityActions';
-import { provinceListAllAction } from '@/actions/ProvinceActions';
 import { categoryListAllAction } from '@/actions/CategoryActions';
 
 
 
 export default async function page() {
-  const [ businessesData, provincesData, citiesData, categoriesData ] = await Promise.all([
+  const [ businessesData, citiesData, categoriesData ] = await Promise.all([
         businessListAction(),  
-        provinceListAllAction(), 
         cityListAllAction(), 
         categoryListAllAction(),
   ]);
