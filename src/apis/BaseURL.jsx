44255719a1db88e8a70d994947f67de340e168dc
Mark Chovava <markchovava@gmail.com>
2025-03-29 14:40:18 +0200

    API

--- src/apis/BaseURL.jsx
+++ src/apis/BaseURL.jsx
@@ -1,2 +1,2 @@
-export const baseURL = 'http://127.0.0.1:8000/';
-//export const baseURL = 'http://143.110.255.30:8002/';
+//export const baseURL = 'http://127.0.0.1:8000/';
+export const baseURL = 'http://143.110.255.30:8002/';
